491566db54ef2b786a821f78f13a663d83911063
gunixr <gunixr@gmail.com>
2015-10-26 16:56:50 +0100

    lun 26 ott 2015, 16.56.50, CET

---
+++ Lab2/es2/es2.c
@@ -0,0 +1,67 @@
+
+// Giuseppe Raffa && Umberto Reale
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <assert.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#define BUFFSIZE 512
+
+void cp_r(char *, char*);
+
+int main (int argc, char *argv[]) { // Copiare il I path nel II path, assumo che il II non esista
+	if(argc!=3) {
+		fprintf(stderr,"Arguments error\n");
+		exit(-1);
+	}
+
+	DIR *dirp1 = opendir(argv[1]);
+	DIR *dirp2 = opendir(argv[2]);
+	if (dirp1==NULL) {
+		fprintf(stderr, "Path 1 error\n");
+		exit(1);
+	}
+	if (dirp2!=NULL) {
+		fprintf(stderr, "Path 2 exist\n");
+		exit(1);
+	}
+	
+	//mkdir
+	cp_r(argv[1],argv[2]);
+
+	return(0);
+}
+
+void cp_r(char *path1,char *path2) {
+	int fp1,fp2,buff[BUFFSIZE];
+	struct stat info;
+	struct dirent *dp;
+	char ptr[1000];
+	char ptr2[1000];
+	DIR *dirp1=opendir(path1); //apro la directory sorgente
+	stat(path1,&info);
+	if(S_ISREG(info.st_mode)) { //usare read e write per copiare file
+		fp1=open(path1,O_RDONLY);
+		fp2=open(path2,O_WRONLY | O_CREAT);
+		while(read(fp1,buff,BUFFSIZE)>0) {
+			write(fp2,buff,BUFFSIZE);
+		}
+		return;
+	}
+	else {
+		mkdir(path2,S_IRWXU | S_IRWXG | S_IRWXO); //chiedere spiegazioni permessi
+		while((dp=readdir(dirp1))!=NULL) {
+			if(dp->d_name[0]!='.') {
+				sprintf(ptr,"%s/%s",path1,dp->d_name);
+				sprintf(ptr2,"%s/%s",path2,dp->d_name);
+				cp_r(ptr,ptr2);
+			}
+		}
+	}		
+}
