d703dba1e1b923d66dc50cea33dfeb58dd570274
robr17 <robertoricci1@msn.com>
2015-11-08 16:52:34 +0100

    alternative version of main_es3.c

---
+++ Lab3/main_es3_1.c
@@ -0,0 +1,56 @@
+//Roberto Ricci
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#define N 10
+#define M 5
+
+/*	Se definito il padre attende tutti gli N figli creati e termina.
+	Se non definito il padre attende gli M<N figli creati e termina.
+*/
+#define switch
+
+int main (int argc, char **argv) {
+
+	int i, status;
+	pid_t pid_v[N], pid;
+
+	for (i=0; i<N; i++) {
+		pid = fork();
+		if (pid < 0) {
+			fprintf(stderr, "Errore nella generazione del processo figlio.\n");
+			exit(EXIT_FAILURE);
+		} else if (pid == 0) {
+			fprintf(stdout, "Termina il processo figlio PID = %d PPID = %d.\n", getpid(), getppid());
+			exit(EXIT_SUCCESS);
+		} else if (pid) {
+			pid_v[i] = pid;
+		}
+	}
+	
+	#ifdef switch
+
+	for (i=0; i<N; i++) {
+		waitpid(pid_v[i], &status, 0);
+		if (!WIFEXITED(status)) {
+			printf("Uno dei processi non è stato eseguito correttamente.\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	#else
+
+	for (i=0; i<M; i++) {
+		waitpid(pid_v[i], &status, 0);
+		if (!WIFEXITED(status)) {
+			printf("Uno dei processi non è stato eseguito correttamente.\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	#endif
+
+	fprintf(stdout, "Termina il processo padre PID = %d.\n", getpid());
+
+	exit(EXIT_SUCCESS);
+}
