67a8e02ffd946fdf7ebbdfbfd0ccd6565d707462
gunixr <gunixr@gmail.com>
2015-11-20 08:44:51 +0100

    es3

--- Lab5/es3/es3.c
+++ Lab5/es3/es3.c
@@ -1,3 +1,4 @@
+//Giuseppe Raffa
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
@@ -8,51 +9,61 @@
 
 void handler(int sig)
 {
-
+	if(sig==SIGUSR1)
+	{
+		int f=open("son1.txt",O_RDONLY);
+		char *buff;
+		buff=(char*)calloc(51,sizeof(char));
+		fprintf(stdout,"Pid1=%d Padre=%d\n",getpid(),getppid());
+		read(f,buff,50);
+		buff=strcat(buff,"\n");
+		write(1,buff,51);
+		close(f);
+		free(buff);
+	}
+	else if(sig==SIGUSR2)
+	{
+		int f=open("son2.txt",O_RDONLY);
+		char *buff;
+		buff=(char*)calloc(51,sizeof(char));
+		fprintf(stdout,"Pid2=%d Padre=%d\n",getpid(),getppid());
+		read(f,buff,50);
+		buff=strcat(buff,"\n");
+		write(1,buff,51);
+		close(f);
+		free(buff);
+		sleep(5);
+	}
+	//invio SIGCHLD
 }
 
 
 int main (int argc, char **argv)
 {
-	pid_t pid;
-	pid=fork();
-	int f1=open("son1.txt",O_RDONLY);
-	int f2=open("son2.txt",O_RDONLY);
-	char *buff1,*buff2;
-	buff1=(char*)calloc(51,sizeof(char));
-	buff2=(char*)calloc(51,sizeof(char));
-	if(pid!=0)
+	signal(SIGUSR1,handler);
+	signal(SIGUSR2,handler);
+	signal(SIGCHLD,handler);
+	pid_t pid1,pid2;
+	pid1=fork();
+	if(pid1!=0)
 	{ //sono nel padre
-		if(fork())
-		{//sono nel padre
-			
-		}
-		else
-		{// secondo figlio
-			fprintf(stdout,"Pid2=%d Padre=%d\n",getpid(),getppid());
-			read(f2,buff2,50);
-			buff2=strcat(buff2,"\n");
-			write(1,buff2,51);
-			close(f2);
-			free(buff2);
-			sleep(5);
-			
-		//exit
+		pid2=fork();
+		if (pid2==0)
+		{	//secondo figlio
+			pause();
+			exit(0);
 		}
 	}
 	else
 	{//primo figlio
-		fprintf(stdout,"Pid1=%d Padre=%d\n",getpid(),getppid());
-		read(f1,buff1,50);
-		buff1=strcat(buff1,"\n");
-		write(1,buff1,51);			
-		close(f1);
-		free(buff1);
-
-	//exit	
-	
+		pause();
+		exit(0);
 	}
+	sleep(5);
+	kill(pid2,SIGUSR2);
+	pause();
+	kill(pid1,SIGUSR1);
+	pause();
+	printf("Padre Termina\n");
 	return 0;
-
-
 }
